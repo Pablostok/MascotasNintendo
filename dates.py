7a6c3e0d93f7f585aa7b7ca73b1affde1f79c941
Pablo V. R <pablitovr007@gmail.com>
2020-01-26 16:00:29 +0100

    Pruebas fechas

--- dates.py
+++ dates.py
@@ -1,6 +1,6 @@
 # 8/6/1993      10:03:13
 
-import datetime
+from datetime import datetime
 
 # hoy = datetime.date.today()
 # print(hoy)
@@ -12,6 +12,21 @@ import datetime
 # print("Minutos: ", t.minute)
 # print("Segundos: ", t.second)
 
-x = datetime.datetime(1993, 6, 8, 10, 3, 13)
+# x = datetime.datetime(1993, 6, 8, 10, 3, 13)
 
-print(x.strftime("%d %b %Y %H:%M:%S"))
+# print(x.strftime("%d %b %Y %H:%M:%S"))
+
+arrayfechas = ['6/7/2020 10:30', "6/7/2020 14:30", "6/7/2020 17:00"]
+
+formato = '%d/%m/%Y  %H:%M'
+
+aux = datetime.strptime(arrayfechas[0], formato)
+
+print(aux)
+
+aux2 = datetime.strptime(arrayfechas[1], formato)
+
+if aux > aux2:
+    print("aux1 > aux2")
+else:
+    print("aux2 > aux1")
